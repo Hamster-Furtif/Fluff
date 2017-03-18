02f1769c4e0afe5fc1037eb83e6f1c114e10f5d2
Hamster-Furtif <lakartoonerie_dutronc@yahoo.fr>
2017-03-19 00:05:04 +0100

    Added matrix and lists.
    That's quite a bit of work but not much to say about.
    Also, I realize you can concat strings with "+", so I don't have to use
    the paineful "StrJoin()" method.

--- src/TxtToCasio.py
+++ src/TxtToCasio.py
@@ -6,11 +6,23 @@ math = "+-*/^="
 cond = "|&<>="
 blank = "\t\n\r"
 includedChars = "\"'_"
+
 var = []
 strings = [[]]
-logic=["&&","||","!!","!|","==","!=","<=",">=","<",">"]
-logic_casio=["And","Or","Not", "Xor","=","È","É","Ê","<",">"]
-transpileWithComments = False
+ls = [[]]
+mats = []
+declarateur = ("Str ", "", "List ", "Mat ")
+
+logic=["&&","||","!!","!|","==","!=","<=",">=","<",">","++","--"]
+logic_casio=["And","Or","Not", "Xor","=","È","É","Ê","<",">","Isz ","Dsz "]
+
+# PARS
+# -c : transpile with comments
+# -i : include blank variable declaration
+#
+
+pars = "" 
+NAME="A"
 
 def transpile(path, twc):
     transpileWithComments = cwc
@@ -24,16 +36,17 @@ def main():
     code = code.split("\n")
     cleanCode()
     write()
-    print(strings)
-    print(var)
-
+    print("Strings:",strings)
+    print("Nums   :", var)
+    print("Lists  :", ls)
+    print("Mats   :", mats)
 
     
          
 def cleanCode():
     i=0
     while i < len(code):
-        if not transpileWithComments and '#' in code[i]:
+        if not "-c" in pars and '#' in code[i]:
             for j in range(0, len(code[i])-1):
                 if code[i][j] == '#':
                     code[i]=code[i][0:j:]
@@ -46,86 +59,78 @@ def cleanCode():
 
 def write():
     global LINES
-    LINES = []
+    LINES = ["Ù\n"]
     queue = []
-    for i in range(0, len(code)):
-        split = splitLine(code[i])
+    global r_line
+    for r_line in range(0, len(code)):
+        split = splitLine(code[r_line])
 
-        ### STR ###
-        if split[0] == "Str":
+
+        ### INIT ###
+        if split[0] in "Num Str List Mat":
             if(len(split)>1 and checkUnused(split[1])==(-1,-1)):
-                strings.append(split[1])
-                txt=concatToString(split[3:])
-                if not txt=="//ERROR":
-                    if "=" in code[i]:
-                        LINES.append(txt+"ãStr "+str(len(strings)))
-                    else:
-                        LINES.append(txt+"Str "+str(len(strings)))
-
-                else:
-                   print("Error at line ", i, ": Num variables can't be used in concatenations")
-
-
-            elif(len(split)==1):
-                print("Error at line ", i, ": name expected after 'Str' declaration")
-
-            else:
-                print("Error at line ", i, ": a variable with the name ", split[1], " is already registered")
-
-        ### NUM ###
-        if split[0] == "Num":
-            if(len(split)>1 and checkUnused(split[1])==(-1,-1)):
-                var.append(split[1])
+                getVarList(split[0]).append(split[1])
+                u=checkUnused(split[1])
                 txt=opToString(split[3:])
                 if not txt=="//ERROR":
-                    if "=" in code[i]:
-                        LINES.append(txt+"ã"+char[len(var)])
+                    if "=" in code[r_line]:
+                        if split[0]in ["Num","Mat"]:
+                            txt= txt.replace('{','[').replace('}',']')  
+                            LINES.append(txt+"ã"+declarateur[u[0]]+char[len(getVarList(split[0]))-1])
+                        else:
+                            LINES.append(txt+"ã"+declarateur[u[0]]+str(len(getVarList(split[0]))-1))
                     else:
-                        LINES.append(txt+char[len(var)])
+                        if split[0]in ["Num","Mat"]:
+                            LINES.append(txt+declarateur[u[0]]+char[len(getVarList(split[0]))-1])
+                        elif "-i" in pars:
+                            LINES.append(declarateur[u[0]]+str(len(getVarList(split[0]))))
 
-                else:
-                   print("Error at line ", i, ": Str variables can't be used in operations")
+       
 
-            elif(len(split)==1):
-                print("Error at line ", i, ": name expected after 'Str' declaration")
-
-            else:
-                print("Error at line ", i, ": a variable with the name ", split[1], " is already registered")
-
-        ### STATEMENT + QUEUE###
-        if split[0] in statement:
+        ### STATEMENT + QUEUE ###
+        elif split[0] in statement:
             cap = split[0][0].capitalize()+split[0][1:]
             LINES+=cap
             LINES+=" "
             LINES+=conditionToString(split[1:])
             queue.append(getStatementEnd(cap))
+            if split[0]=="if":
+                LINES[-1]+="Ù\nThen "
 
-        if split[0]=='}':
+        elif split[0]=='}':
             LINES+=queue.pop()
-        
 
-        LINES[-1]+="Ù\n"
+        ### OPERATIONS AND VAR FUNCTIONS
+        elif checkUnused(split[0])[0] != -1 and len(splitLine)>2:
+            u = checkUnused(split[0])
+
+            ## OPERATIONS ##
+            if splitLine[1]=='=':
+                if u[0]==1:
+                    if(split[1]in ["++","--"]):
+                        LINES+=opToString(split)
+                    else:
+                        LINES+=opToString(split[2:])+"ã"+char[var.index(split[0])]
+                elif u[0]==0:
+                    LINES+=opToString(split[2:])+"ãStr "+str(strings.index(split[0]))
+                elif u[0]==2:
+                    LINES+=opToString(split[2:])+"ãList "+str(ls.index(split[0]))
+                elif u[0]==3:
+                    LINES+=opToString(split[2:])+"ãMat "+char[mats.index(split[0])]
+
+
+            ## FUNCTIONS ##
+            elif splitLine[1]=='.':
+                
+                           
+        
+        if(LINES[-1][-5:] != "Then "):
+            LINES[-1]+="Ù\n"
         
     file = open("casio.txt",'w', encoding="utf8")
     file.writelines(LINES)
     file.close()
-    
-    
-
-        
-    
-def getNumIndex(name):
-    for i in range (0, len(var)):
-        if var[i][0]==name:
-            return i
-    return -1
-            
-def getStrIndex(name):
-    if len(strings)>0:
-        for i in range (1, len(strings)):
-            if strings[i][0]==name:
-                return i
-    return -1
+                                                         
 
 def checkUnused(name):
 
@@ -135,6 +140,13 @@ def checkUnused(name):
     for i in range(0, len(var)):
         if name == var[i]:
             return (1,i)
+    for i in range(1, len(ls)):
+        if name == ls[i]:
+            return (2,i)
+    for i in range(0, len(mats)):
+        if name == mats[i]:
+            return (3,i)
+    
     if name in forbiden:
         return (2,-1)
     #(tab, index)
@@ -166,79 +178,69 @@ def splitLine(string):
 
         elif(string[i] in math+cond):
             temp=''
-            while i<len(string) and string[i] in math+cond+"(){}.":
+            while i<len(string) and string[i] in math+cond:
                 temp += string[i]
                 i+=1
             result.append(temp)
-
-        elif(string[i] in "(){}."):
+        
+        elif(string[i] in "{}()."):
             result.append(string[i])
             i+=1
         
         elif(string[i] in blank):
             i+=1
+        elif(string[i] in '['):
+            temp = '['
+            i+=1
+            while i<len(string) and  string[i] not in '][':
+                temp+=string[i]
+                i+=1
+            if i<len(string) and string[i]==']':
+                temp+=']'
+                i+=1
+            result.append(temp)
+        elif(string[i]==']'):
+            result.append(']')
+            i+=1
+            
+            
         else:
             # Error, unexpected character
             i+=1
     return result
 
-def opToString(split):
+def opToString(split, *rec):
     final=''
     par="+-ÀÁ^="
-    for i in range(0, len(split)):
-        u=checkUnused(split[i]) 
-        if u[0]==0:
-            final="//ERROR"
-            break
-        elif(u[0]==1):
-            final+=char[u[1]]
-        elif(split[i] in math):
-            final+= par[math.index(split[i])]
-        else:
-            final+=split[i].replace('i','½')
+    if len(split)==2 and checkUnused(split[0])[0]==1 and split[1] in ["++","--"]:
+        final = dictionary(split[1])+char[var.index(split[0])]
+    else:
+        for i in range(0, len(split)):
+            u=checkUnused(split[i]) 
+            if u[0]==0:
+                final+="Str "+str(u[1])
+            elif(u[0]==1):
+                final+=char[u[1]]
+            elif(split[i] in math):
+                final+= par[math.index(split[i])]
+            elif(split[i][0]=='[' and split[i][-1]==']'):
+                final+="{"
+                final+=opToString(splitLine(split[i][1:-1]), ',')
+                final+='}'
+            elif split[i]in['[',']']:
+                final+="{}"[['[',']'].index(split[i])]
+            else:
+                final+=split[i].replace('i','½')
+            if rec != () and i!=len(split)-1:
+                 final+=rec[0]
     
     return final
             
-
-def concatToString(split):
-    par=0
-    final=""
-    if len(split)==1:
-        if split[0][0]=='"' and split[0][-1]=='"':
-            return split[0]
-        elif checkUnused(split[0])[0]==0:
-            return "Str "+str(checkUnused(split[0])[1])
-    elif split[1]=='+' and len(split)>=3:
-        for i in range(0, len(split),2):
-            if i+2<len(split) and split[i+1]=="+":
-                if split[i][0]=='"' and split[i][-1]=='"':
-                    final+="StrJoin("+split[i]+","
-                    par+=1
-                elif checkUnused(split[i])[0]==0:
-                    final+="StrJoin(Str "+str(checkUnused(split[i])[1])+","
-                    par+=1
-                else:
-                    return "//ERROR_A"
-            elif i==len(split)-1:
-                if split[i][0]=='"' and split[i][-1]=='"':
-                    final+=split[i]
-                elif checkUnused(split[i])[0]==0:
-                    final+="Str "+str(checkUnused(split[i])[1])
-                else:
-                    return "//ERROR_B"
-                
-            else:
-
-                return "//ERROR_C"
-        final+=")"*par
-
-        return final
 
 def conditionToString(split):
     final = ""
     
     for i in range(0,len(split)):
-        print(split[i])
         
         if split[i][0]==split[i][-1]=='"' or checkUnused(split[i])[0]==0:
             c_split=[]
@@ -269,9 +271,22 @@ def getStatementEnd(string):
         return "Next"
     else:
         return ""
+    
 def dictionary(string):
     for i in range(0, len(logic)):
         if string == logic[i]:
             return logic_casio[i]
     return ""
             
+def getVarList(name):
+    u=checkUnused(name)
+    if u[0]==0 or name=="Str":
+        return strings
+    elif u[0]==1 or name=="Num":
+        return var
+    elif u[0]==2 or name=="List":
+        return ls
+    elif u[0]==3 or name=="Mat":
+        return mats
+    else:
+        return None
